7f15ce4d6ae8880ea53c8f26dde498ce98635d3b
Kushan Dimantha <kushandimantha533@gmail.com>
2023-11-24 13:22:14 +0530

    first commit

---
+++ app/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <Link href="/" className="btn btn-ghost text-xl">Home</Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link href="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="p-2 bg-base-100">
+                  <li>
+                    <a>Link 1</a>
+                  </li>
+                  <li>
+                    <a>Link 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

3fad840b5e7a27fe31e67b80f27e1da72c016e63
Diniru Sandipa <it20041120@my.sliit.lk>
2023-11-27 16:16:46 +0530

    coverphoto

--- app/components/coverphoto/coverphoto.jsx
+++ app/components/coverphoto/coverphoto.jsx
@@ -1,16 +1,51 @@
 import React from "react";
-import Image from 'next/image';
+import Image from "next/image";
+import Search from "./search";
 
 const Coverphoto = () => {
   return (
-    <div class="coverphoto">
-        <div style={{position:'absolute'}}>
-            <Image src="/banner-image.png" alt="Honeymoon Logo" width={1440} height={514}/>
-        </div> 
-        asas
+    <div className="coverphoto" style={{ position: 'relative' }}>
+      <Image
+        src="/banner-image.png"
+        alt="Honeymoon Logo"
+        layout="responsive"
+        width={100}
+        height={514}
+        style={{ filter: 'brightness(50%)' }} // Adjust the brightness value
+      />
+      <div
+        style={{
+          position: 'absolute', 
+          top: 0, 
+          left: 0, 
+          width: '100%', 
+          height: '75%', 
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          color: 'white',
+          fontSize:'54px',
+          fontWeight:'700',
+        }}
+      >
+        <h1>Your Love Story</h1>
+        <h1>Deserves the Best</h1>
+      </div>
+      <div
+      style={{
+        position: 'absolute', 
+        top: 280, 
+        left: 300, 
+        width: '948px', 
+        height: '100%', 
+      }}
+      >
+        <Search/>
+      </div>
+      
     </div>
   );
-  
 };
 
 export default Coverphoto;
